3e96db6e6ce357d21d121c0a5174bcd3da84a6a7
Levin <github@17d.me>
2025-04-03 23:21:46 +0200

    Click dummy for location list

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -13,14 +13,14 @@ const showingNavigationDropdown = ref(false);
             class="container mx-auto mb-2 max-w-screen-xl grow px-0 pt-0 sm:px-5 md:pt-8"
         >
             <NavBar />
-            <div class="mt-12 grid grid-cols-12 gap-4">
+            <div class="grid grid-cols-12 gap-4 md:mt-12">
                 <div class="col-span-12 lg:col-span-3 lg:col-start-2">
                     <SideMenu class="hidden md:flex" />
                 </div>
                 <div
                     class="col-span-12 md:flex md:justify-center lg:col-span-7"
                 >
-                    <main class="w-full">
+                    <main class="mb-12 w-full pb-12 md:mb-0 md:pb-0">
                         <main class="md:flex md:justify-center">
                             <slot />
                         </main>
