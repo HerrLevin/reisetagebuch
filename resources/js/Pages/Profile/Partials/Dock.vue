1585d1af1067dd15f0b29a9dbedef1e88ccd44af
Levin <github@17d.me>
2025-04-03 23:29:12 +0200

    Icons + better location list hover

--- resources/js/Pages/Profile/Partials/Dock.vue
+++ resources/js/Pages/Profile/Partials/Dock.vue
@@ -10,42 +10,18 @@ import { Link } from '@inertiajs/vue3';
             as="button"
         >
             <svg
-                class="size-[1.2em]"
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-[1.2em]"
             >
-                <g
-                    fill="currentColor"
-                    stroke-linejoin="miter"
-                    stroke-linecap="butt"
-                >
-                    <polyline
-                        points="1 11 12 2 23 11"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-miterlimit="10"
-                        stroke-width="2"
-                    ></polyline>
-                    <path
-                        d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="square"
-                        stroke-miterlimit="10"
-                        stroke-width="2"
-                    ></path>
-                    <line
-                        x1="12"
-                        y1="22"
-                        x2="12"
-                        y2="18"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="square"
-                        stroke-miterlimit="10"
-                        stroke-width="2"
-                    ></line>
-                </g>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
             </svg>
             <span class="dock-label">Home</span>
         </Link>
@@ -61,7 +37,7 @@ import { Link } from '@inertiajs/vue3';
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                class="size-6"
+                class="size-[1.2em]"
             >
                 <path
                     stroke-linecap="round"
@@ -78,35 +54,25 @@ import { Link } from '@inertiajs/vue3';
             :class="{ 'dock-active': route().current('profile.edit') }"
         >
             <svg
-                class="size-[1.2em]"
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-[1.2em]"
             >
-                <g
-                    fill="currentColor"
-                    stroke-linejoin="miter"
-                    stroke-linecap="butt"
-                >
-                    <circle
-                        cx="12"
-                        cy="12"
-                        r="3"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="square"
-                        stroke-miterlimit="10"
-                        stroke-width="2"
-                    ></circle>
-                    <path
-                        d="m22,13.25v-2.5l-2.318-.966c-.167-.581-.395-1.135-.682-1.654l.954-2.318-1.768-1.768-2.318.954c-.518-.287-1.073-.515-1.654-.682l-.966-2.318h-2.5l-.966,2.318c-.581.167-1.135.395-1.654.682l-2.318-.954-1.768,1.768.954,2.318c-.287.518-.515,1.073-.682,1.654l-2.318.966v2.5l2.318.966c.167.581.395,1.135.682,1.654l-.954,2.318,1.768,1.768,2.318-.954c.518.287,1.073.515,1.654.682l.966,2.318h2.5l.966-2.318c.581-.167,1.135-.395,1.654-.682l2.318.954,1.768-1.768-.954-2.318c.287-.518.515-1.073.682-1.654l2.318-.966Z"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="square"
-                        stroke-miterlimit="10"
-                        stroke-width="2"
-                    ></path>
-                </g>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+                />
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
             </svg>
+
             <span class="dock-label">Settings</span>
         </Link>
     </div>
