dbae8f933f98f865be928babad51f500416d81c7
Levin <github@17d.me>
2025-04-24 11:42:24 +0200

    click dummy: side menu / dock menu for departures

--- resources/js/Layouts/Partials/SideMenu.vue
+++ resources/js/Layouts/Partials/SideMenu.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import Cog from '@/Icons/Cog.vue';
+import House from '@/Icons/House.vue';
+import Pin from '@/Icons/Pin.vue';
+import PlusCircle from '@/Icons/PlusCircle.vue';
 import { LocationService } from '@/Services/LocationService';
 import { Link } from '@inertiajs/vue3';
 import { onMounted, ref } from 'vue';
+import List from '@/Icons/List.vue';
+import PencilSquare from '@/Icons/PencilSquare.vue';
 
 const latitude = ref(0);
 const longitude = ref(0);
@@ -12,6 +18,24 @@ onMounted(() => {
         longitude.value = position.coords.longitude;
     });
 });
+const isPostsCreateRoute = () => {
+    return route().current()?.startsWith('posts.create');
+};
+const isDashboardRoute = () => {
+    return route().current('dashboard');
+};
+const isSettingsRoute = () => {
+    return route().current('profile.edit');
+};
+const isVenueRoute = () => {
+    return route().current('posts.create.start');
+};
+const isDeparturesRoute = () => {
+    return route().current('posts.create.departures');
+};
+const isTextRoute = () => {
+    return route().current('posts.create.text');
+};
 </script>
 
 <template>
@@ -19,22 +43,9 @@ onMounted(() => {
         <li>
             <Link
                 :href="route('dashboard')"
-                :class="{ 'menu-active': route().current('dashboard') }"
+                :class="{ 'menu-active': isDashboardRoute() }"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-5 w-5"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                    />
-                </svg>
+                <House class="h-5 w-5" />
 
                 Dashboard
             </Link>
@@ -47,53 +58,62 @@ onMounted(() => {
                         longitude: longitude,
                     })
                 "
-                :class="{
-                    'menu-active': route()
-                        .current()
-                        ?.startsWith('posts.create'),
-                }"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-5 w-5"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                    />
-                </svg>
+                <PlusCircle class="h-5 w-5" />
                 <span class="dock-label">New Post</span>
             </Link>
+            <ul v-if="isPostsCreateRoute()">
+                <li>
+                    <Link
+                        :href="
+                            route('posts.create.start', {
+                                latitude: latitude,
+                                longitude: longitude,
+                            })
+                        "
+                        :class="{ 'menu-active': isVenueRoute() }"
+                    >
+                        <Pin class="h-5 w-5" />
+                        Locations
+                    </Link>
+                </li>
+
+                <li>
+                    <Link
+                        :href="
+                            route('posts.create.departures', {
+                                latitude: latitude,
+                                longitude: longitude,
+                            })
+                        "
+                        :class="{ 'menu-active': isDeparturesRoute() }"
+                    >
+                        <List class="h-5 w-5" />
+                        Departures
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        :href="
+                            route('posts.create.text', {
+                                latitude: latitude,
+                                longitude: longitude,
+                            })
+                        "
+                        :class="{ 'menu-active': isTextRoute() }"
+                    >
+                        <PencilSquare class="h-5 w-5" />
+                        Text
+                    </Link>
+                </li>
+            </ul>
         </li>
         <li>
             <Link
                 :href="route('profile.edit')"
-                :class="{ 'menu-active': route().current('profile.edit') }"
+                :class="{ 'menu-active': isSettingsRoute() }"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-5 w-5"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
-                    />
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                    />
-                </svg>
+                <Cog class="h-5 w-5" />
                 Settings
             </Link>
         </li>
