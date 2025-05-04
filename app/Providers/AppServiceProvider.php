b1e4182cefb8175dcdd9edfc4343731e41927344
Levin <github@17d.me>
2025-05-04 18:04:38 +0200

    fix HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         JsonResource::withoutWrapping();
         Vite::prefetch(concurrency: 3);
+
+        // This will force all links to be with https in production
+        // This is useful when you are using a load balancer or reverse proxy
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
