857f2d5eef0be813dd3d79fb6eea15f575b23387
Levin <github@17d.me>
2025-05-04 15:41:18 +0200

    remove repo from frontend

--- app/Http/Controllers/Inertia/UserController.php
+++ app/Http/Controllers/Inertia/UserController.php
@@ -2,25 +2,27 @@
 
 namespace App\Http\Controllers\Inertia;
 
+use App\Http\Controllers\Backend\PostController;
 use App\Http\Controllers\Backend\UserController as BackendUserController;
-use App\Repositories\PostRepository;
 use Inertia\Response;
 use Inertia\ResponseFactory;
 
 class UserController extends Controller
 {
     private BackendUserController $userController;
+    private PostController $postController;
 
-    public function __construct(BackendUserController $userController)
+    public function __construct(BackendUserController $userController, PostController $postController)
     {
+        $this->postController = $postController;
         $this->userController = $userController;
     }
 
     public function show(string $username): Response|ResponseFactory
     {
         $user = $this->userController->show($username);
+        $posts = $this->postController->postsForUser($user->id);
 
-        $posts = new PostRepository()->dashboard($user->id);
 
         return inertia('Profile/Show', [
             'userId' => $user->id,
