4800907b0904b1a734073f66a41d53c4577c03f6
Levin <github@17d.me>
2025-05-09 00:04:06 +0200

    Share button

--- resources/js/Components/Post/Interactions.vue
+++ resources/js/Components/Post/Interactions.vue
@@ -2,24 +2,59 @@
 import ContextMenu from '@/Components/Post/ContextMenu.vue';
 import ArrowUpOnSquare from '@/Icons/ArrowUpOnSquare.vue';
 import Heart from '@/Icons/Heart.vue';
-import { BasePost } from '@/types/PostTypes';
+import {
+    BasePost,
+    isLocationPost,
+    isTransportPost,
+    LocationPost,
+    TransportPost,
+} from '@/types/PostTypes';
 import { PropType } from 'vue';
 
-defineProps({
+const props = defineProps({
     post: {
-        type: Object as PropType<BasePost>,
+        type: Object as PropType<BasePost | TransportPost | LocationPost>,
         required: true,
     },
 });
+
+function sharePost(): void {
+    let postText = `Check out ${props.post.user.name}'s post!`;
+    if (isLocationPost(props.post)) {
+        postText = `Check out ${props.post.user.name}'s post at ${props.post.location.name}!`;
+    } else if (isTransportPost(props.post)) {
+        postText = `Check out ${props.post.user.name}'s travel from ${props.post.start.name} to ${props.post.stop.name}`;
+    }
+    const shareData = {
+        title: 'Post',
+        text: postText,
+        url: route('posts.show', props.post.id),
+    };
+
+    if (navigator.canShare && navigator.canShare(shareData)) {
+        navigator.share(shareData).then().catch();
+    } else {
+        // copy the link to the clipboard
+        navigator.clipboard
+            .writeText(shareData.url)
+            .then(() => {
+                alert('Post link copied to clipboard');
+            })
+            .catch();
+    }
+}
 </script>
 
 <template>
     <div class="flex items-center justify-between">
-        <button class="btn btn-ghost btn-sm btn-circle">
+        <button class="btn btn-ghost btn-sm btn-circle hidden">
             <Heart />
         </button>
 
-        <button class="btn btn-ghost btn-sm btn-circle">
+        <button
+            class="btn btn-ghost btn-sm btn-circle"
+            @click.prevent="sharePost()"
+        >
             <ArrowUpOnSquare />
         </button>
 
