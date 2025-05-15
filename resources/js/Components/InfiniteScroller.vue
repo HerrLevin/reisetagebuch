86b66a067aab704347413153a2df12b5655aea6f
Levin <github@17d.me>
2025-05-15 22:06:13 +0200

    Fix infinite scrolling

--- resources/js/Components/InfiniteScroller.vue
+++ resources/js/Components/InfiniteScroller.vue
@@ -7,32 +7,38 @@ const props = defineProps({
         default: () => ['posts'],
     },
 });
-const page = usePage();
 
 const onlyValue = props.only;
 onlyValue.push('nextCursor');
+
+function cursor(cursor: string | unknown) {
+    if (typeof cursor !== 'string') {
+        return null;
+    }
+
+    return cursor?.length > 5 ? cursor : null;
+}
+
+console.log('cursor', cursor(usePage().props.nextCursor));
+console.log(usePage().props);
 </script>
 
 <template>
     <WhenVisible
-        :always="page.props.nextCursor !== null"
+        v-if="cursor(usePage().props.nextCursor) !== null"
+        :always="cursor(usePage().props.nextCursor) !== null"
         :params="{
             data: {
-                cursor:
-                    typeof page.props.nextCursor === 'string'
-                        ? page.props.nextCursor
-                        : null,
+                cursor: cursor(usePage().props.nextCursor),
             },
             only: ['posts', 'nextCursor'],
         }"
     >
-        <div v-if="page.props.nextCursor === null" class="p-4 pb-8 text-center">
-            You've reached the end!
-        </div>
-        <div v-else class="p-4 pb-8 text-center">
+        <div class="p-4 pb-8 text-center">
             <span class="loading loading-infinity loading-md"></span>
         </div>
     </WhenVisible>
+    <div v-else class="p-4 pb-8 text-center">You've reached the end!</div>
 </template>
 
 <style scoped></style>
