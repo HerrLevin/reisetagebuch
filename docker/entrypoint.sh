2d197811e8aa934f43abf0f0718433de4ea65bac
Levin <github@17d.me>
2025-07-22 22:11:23 +0200

    Add Dockerfile + GitHub workflow

---
+++ docker/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Exit script on error
+set -e
+
+echo "Starting Reisetagebuch..."
+
+if [ ! -f /var/www/html/.env ]; then
+  echo "Environment file not found. Creating a new one..."
+  touch /var/www/html/.env
+  echo "APP_ENV=production" >> /var/www/html/.env
+  echo "APP_DEBUG=false" >> /var/www/html/.env
+  echo "APP_KEY=base64:$(openssl rand -base64 32)" > /var/www/html/.env
+fi
+
+# Set correct permissions
+chown -R www-data:www-data /var/www/html/database/database.sqlite
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Start Supervisor in the background
+supervisord -c /etc/supervisord.conf &
+
+# Run database migrations and other artisan commands
+echo "Running Laravel artisan commands..."
+
+# echo "Running migrations..."
+php artisan migrate --force
+
+echo "Clearing caches..."
+php artisan optimize
+
+echo "Restart queue workers..."
+php artisan queue:restart
+
+# Keep the container running
+wait
