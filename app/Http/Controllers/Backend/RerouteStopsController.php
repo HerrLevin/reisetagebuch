cb774e865faf1b80a7de763ad653f0586feb86f2
Levin <github@17d.me>
2025-10-15 21:00:33 +0200

    🐛 Fix RerouteStops Job

--- app/Http/Controllers/Backend/RerouteStopsController.php
+++ app/Http/Controllers/Backend/RerouteStopsController.php
@@ -14,6 +14,7 @@ use App\Repositories\TransportTripRepository;
 use App\Services\BrouterRequestService;
 use Clickbar\Magellan\IO\Parser\Geojson\GeojsonParser;
 use GuzzleHttp\Exception\GuzzleException;
+use Log;
 
 class RerouteStopsController extends Controller
 {
@@ -51,16 +52,20 @@ class RerouteStopsController extends Controller
                 $mode = $tripDto->mode;
             }
 
-            $pathType = $this->getPathType($mode);
+            $pathType = $this->getPathType(TransportMode::tryFrom($mode));
+            if (! $pathType) {
+                Log::warning('RerouteStops: Unsupported transport mode, skipping', ['mode' => $mode]);
+
+                continue;
+            }
+            Log::debug('RerouteStops: Transport mode', ['mode' => $mode, 'pathType' => $pathType]);
             $this->rerouteBetween($previousStop, $stop, $pathType);
         }
     }
 
-    private function rerouteBetween(TransportTripStop $start, TransportTripStop $end, ?string $pathType): void
+    private function rerouteBetween(TransportTripStop $start, TransportTripStop $end, string $pathType): void
     {
-        if (! $pathType) {
-            return;
-        }
+        Log::debug('RerouteStops', [$start, $end, $pathType]);
 
         $startTime = $start->departure_time ?? $start->arrival_time;
         $endTime = $end->arrival_time ?? $end->departure_time;
@@ -68,15 +73,22 @@ class RerouteStopsController extends Controller
         $duration = -1;
         if ($startTime && $endTime) {
             $duration = (int) round($startTime->diffInSeconds($endTime));
+            Log::debug('RerouteStops', [$duration, $pathType]);
         }
 
         $segment = $this->transportTripRepository->getRouteSegmentBetweenStops($start, $end, $duration, $pathType);
         if ($segment) {
+            Log::debug('RerouteStops: Segment already exists, setting for stop', ['segment' => $segment->id]);
             $this->transportTripRepository->setRouteSegmentForStop($start, $segment);
 
             return; // already rerouted
         }
         try {
+            Log::debug('Getting new route from BRouter', [
+                'from' => $start->location->location,
+                'to' => $end->location->location,
+                'type' => $pathType,
+            ]);
             $route = $this->brouterRequestService->getRoute($start->location->location, $end->location->location, $pathType);
         } catch (BrouterRouteCreationFailed|GuzzleException $e) {
             report($e);
@@ -89,11 +101,12 @@ class RerouteStopsController extends Controller
             $segment = $this->transportTripRepository->createRouteSegment($start->location, $end->location, $duration, $pathType, $route);
             $this->transportTripRepository->setRouteSegmentForStop($start, $segment);
         } catch (\Exception $e) {
+            Log::error('RerouteStops: Failed to create route segment', ['error' => $e->getMessage()]);
             report($e);
         }
     }
 
-    private function getPathType(string $mode): ?string
+    private function getPathType(?TransportMode $mode): ?string
     {
         $railModes = [
             TransportMode::RAIL,
