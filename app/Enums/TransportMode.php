3ed26b3782abe3ad62af4ff8d3e931050890f3cc
Levin <github@17d.me>
2025-10-15 20:02:18 +0200

    ✨ Feature: Crosspost manual trips to TRWL

--- app/Enums/TransportMode.php
+++ app/Enums/TransportMode.php
@@ -24,5 +24,23 @@ enum TransportMode: string
     case NIGHT_RAIL = 'NIGHT_RAIL';
     case REGIONAL_FAST_RAIL = 'REGIONAL_FAST_RAIL';
     case REGIONAL_RAIL = 'REGIONAL_RAIL';
+    case FUNICULAR = 'FUNICULAR';
     case OTHER = 'OTHER';
+    case FLEX = 'FLEX';
+
+    public function getTraewellingType(): string
+    {
+        return match ($this) {
+            self::HIGHSPEED_RAIL, self::REGIONAL_FAST_RAIL, self::NIGHT_RAIL => 'nationalExpress',
+            self::LONG_DISTANCE => 'regionalExp',
+            self::METRO => 'suburban',
+            self::BUS, self::COACH => 'bus',
+            self::FERRY => 'ferry',
+            self::SUBWAY => 'subway',
+            self::TRAM, self::FUNICULAR => 'tram',
+            self::AIRPLANE => 'plane',
+            self::CAR, self::CAR_PARKING, self::ODM, self::RENTAL, self::FLEX => 'taxi',
+            default => 'regional'
+        };
+    }
 }
--- app/Jobs/TraewellingCrossCheckInJob.php
+++ app/Jobs/TraewellingCrossCheckInJob.php
@@ -2,15 +2,21 @@
 
 namespace App\Jobs;
 
+use App\Enums\TransportMode;
+use App\Models\Location;
 use App\Models\LocationIdentifier;
 use App\Models\Post;
 use App\Models\PostMetaInfo;
 use App\Models\SocialAccount;
+use App\Models\TransportTripStop;
+use Carbon\Carbon;
+use Clickbar\Magellan\Data\Geometries\Point;
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\Token;
@@ -31,6 +37,7 @@ class TraewellingCrossCheckInJob implements ShouldQueue
 
     /**
      * @throws GuzzleException
+     * @throws Exception|Throwable
      */
     public function handle(): void
     {
@@ -38,51 +45,247 @@ class TraewellingCrossCheckInJob implements ShouldQueue
         $hasTrwlMeta = $post?->metaInfos->where('key', 'traewelling_trip_id')->count() > 0;
         $isTrwlUser = SocialAccount::whereUserId($post?->user->id)->whereProvider('traewelling')->exists();
 
-        if (! $isTrwlUser || $hasTrwlMeta || $post?->transportPost?->transportTrip?->provider !== 'transitous') {
+        if (! $isTrwlUser || $hasTrwlMeta) {
             Log::debug('Skipping Traewelling check-in for post '.$this->postId);
 
             return;
         }
-        $this->getAccessToken($post);
 
-        $originStop = $post->transportPost->originStop;
-        $destinationStop = $post->transportPost->destinationStop;
+        if ($post->transportPost->transportTrip->provider === 'transitous') {
+            $data = $this->crosspostTransitous($post);
+        } elseif ($post->transportPost->transportTrip->provider === 'reisetagebuch') {
+            $data = $this->crosspostManualTrip($post);
+        } else {
+            Log::error('Unknown trip provider: '.$post->transportPost->transportTrip->provider);
 
-        // todo: get from database
-        $trwlOriginIdentifier = $this->getTrwlStationFromIdentifier($originStop->location->identifiers->firstWhere('origin', 'motis')?->identifier ?? '');
-        $trwlDestinationIdentifier = $this->getTrwlStationFromIdentifier($destinationStop->location->identifiers->firstWhere('origin', 'motis')?->identifier ?? '');
-        if (! $trwlOriginIdentifier || ! $trwlDestinationIdentifier) {
-            throw new Exception('Origin departure board not found');
-        }
-
-        $trwlOrigin = LocationIdentifier::updateOrCreate(
-            ['origin' => 'traewelling', 'type' => 'stop', 'location_id' => $originStop->location->id],
-            ['identifier' => $trwlOriginIdentifier['id'], 'name' => $trwlOriginIdentifier['name']]
-        );
-        $trwlDestination = LocationIdentifier::updateOrCreate(
-            ['origin' => 'traewelling', 'type' => 'stop', 'location_id' => $destinationStop->location->id],
-            ['identifier' => $trwlDestinationIdentifier['id'], 'name' => $trwlDestinationIdentifier['name']]
-        );
-
-        // post to traewelling
-        $data = $this->checkin($post, $trwlOrigin, $trwlDestination);
-        if (isset($data['error'])) {
-            throw new Exception('Traewelling API error: '.$data['error']);
+            return;
         }
 
         $meta = new PostMetaInfo;
         $meta->post_id = $post->id;
         $meta->key = 'traewelling_trip_id';
-        $meta->value = $data['data']['id'] ?? '';
+        $meta->value = $data['status']['id'] ?? '';
         $meta->save();
+        Log::debug('Created Traewelling check-in for post '.$this->postId, ['response' => $data, 'meta' => $meta]);
     }
 
-    private function getTrwlStationFromIdentifier(string $identifier): ?array
+    private function getTrwlStationFromLocation(Point $point): ?array
+    {
+        $client = $this->getClient();
+        try {
+            $response = $client->get('trains/station/nearby', [
+                'query' => [
+                    'latitude' => $point->getY(),
+                    'longitude' => $point->getX(),
+                ],
+            ]);
+        } catch (GuzzleException $e) {
+            Log::error('Error fetching nearby station from Traewelling', ['exception' => $e->getMessage(), 'point' => $point]);
+
+            return null;
+        }
+
+        $data = json_decode($response->getBody()->getContents(), true);
+        $location = $data['data'] ?? null;
+        Log::debug('Station nearby response', ['response' => $data, 'point' => $point, 'location' => $location]);
+
+        return $location;
+    }
+
+    private function getTrwlStationIdentifier(Location $location): ?LocationIdentifier
+    {
+        Log::debug('Getting TRWL-id for location '.$location->name, ['location_id' => $location->id]);
+        $traewellingIdentifier = $location->identifiers->firstWhere('origin', 'traewelling') ?? null;
+        if ($traewellingIdentifier !== null) {
+            Log::debug('Found existing TRWL-id for location '.$location->name, ['identifier' => $traewellingIdentifier]);
+
+            return $traewellingIdentifier;
+        }
+
+        $stationMotisIdentifier = $location->identifiers->firstWhere('origin', 'motis') ?? null;
+        if ($stationMotisIdentifier !== null) {
+            $trwlStation = $this->getTrwlStationFromIdentifier($stationMotisIdentifier);
+            if ($trwlStation !== null) {
+                Log::debug('Found TRWL-id from MOTIS identifier for location '.$location->name, ['identifier' => $trwlStation]);
+
+                return LocationIdentifier::updateOrCreate(
+                    ['origin' => 'traewelling', 'type' => 'stop', 'location_id' => $location->id],
+                    ['identifier' => $trwlStation['id'], 'name' => $trwlStation['name']]
+                );
+            }
+        }
+
+        $trwlStation = $this->getTrwlStationFromLocation($location->location);
+        if ($trwlStation !== null) {
+            Log::debug('Found TRWL-id from location for location '.$location->name, ['identifier' => $trwlStation]);
+
+            return LocationIdentifier::updateOrCreate(
+                ['origin' => 'traewelling', 'type' => 'stop', 'location_id' => $location->id],
+                ['identifier' => $trwlStation['id'], 'name' => $trwlStation['name']]
+            );
+        }
+
+        Log::debug('No TRWL-id found for location '.$location->name);
+
+        return null;
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws Throwable
+     */
+    private function crosspostManualTrip(Post $post): ?array
+    {
+        $this->getAccessToken($post);
+        $originStop = $post->transportPost->originStop;
+        $destinationStop = $post->transportPost->destinationStop;
+
+        $trwlOriginIdentifier = $this->getTrwlStationIdentifier($originStop->location);
+        $trwlDestinationIdentifier = $this->getTrwlStationIdentifier($destinationStop->location);
+        if (! $trwlOriginIdentifier || ! $trwlDestinationIdentifier) {
+            throw new Exception('Origin or destination departure board not found');
+        }
+
+        // post to traewelling
+        $trip = $this->createTrip($post, $trwlOriginIdentifier, $trwlDestinationIdentifier);
+
+        return $this->checkin($post, $trwlOriginIdentifier, $trwlDestinationIdentifier, false, $trip['data']['id'] ?? null);
+    }
+
+    /**
+     * @param  Collection<int, TransportTripStop>  $stopovers
+     */
+    private function createStopovers(Collection $stopovers): Collection
+    {
+        Log::debug('Creating stopovers');
+        Log::debug(sprintf('Stopovers count: %d', $stopovers->count()));
+        if ($stopovers->count() <= 2) {
+            return collect();
+        }
+        // if no times are given, interpolate them evenly between origin and destination
+        $firstStop = $stopovers->first();
+        $lastStop = $stopovers->last();
+        $totalDuration = $firstStop->departure_time && $lastStop->arrival_time ?
+            $firstStop->departure_time->diffInSeconds($lastStop->arrival_time) : null;
+        Log::debug('Total duration: '.($totalDuration ?? 'null'));
+
+        $numStops = $stopovers->count() - 1;
+        // remove first and last stop from stopovers
+        $stopovers = $stopovers->slice(1, $stopovers->count() - 2);
+        Log::debug('Number of intermediate stops: '.$numStops);
+
+        return $stopovers->map(function (TransportTripStop $stop, int $index) use ($firstStop, $totalDuration, $numStops) {
+            $interpolatedArrival = null;
+            $interpolatedDeparture = null;
+            if ($totalDuration !== null && $numStops > 0) {
+                // interpolate evenly
+                $interpolatedArrival = $firstStop->departure_time?->copy()->addSeconds(intval($totalDuration * ($index + 1) / ($numStops + 1)));
+                $interpolatedDeparture = $interpolatedArrival?->copy()->addMinutes(2); // assume 2 minutes stopover
+                Log::debug(sprintf('Interpolated stop %d: arrival %s, departure %s', $index, $interpolatedArrival?->toIso8601String(), $interpolatedDeparture?->toIso8601String()));
+            }
+
+            return $this->createStopover($stop, $interpolatedDeparture, $interpolatedArrival);
+        })->filter(fn ($stop) => $stop !== null);
+
+    }
+
+    private function createStopover(TransportTripStop $stop, ?Carbon $interpolatedDeparture = null, ?Carbon $interpolatedArrival = null): ?array
+    {
+        $trwlStation = $this->getTrwlStationIdentifier($stop->location);
+        if (! $trwlStation) {
+            Log::debug('Stopover station '.$stop->location->name.' not found');
+
+            return null;
+        }
+
+        return [
+            'stationId' => (int) $trwlStation->identifier,
+            'arrival' => $interpolatedArrival?->toIso8601String() ?? $stop->arrival_time?->toIso8601String() ?? null,
+            'departure' => $interpolatedDeparture?->toIso8601String() ?? $stop->departure_time?->toIso8601String() ?? null,
+        ];
+    }
+
+    private function getLineName(Post $post): string
+    {
+        if ($post->transportPost->transportTrip->line_name) {
+            return $post->transportPost->transportTrip->line_name;
+        }
+
+        if ($post->transportPost->transportTrip->trip_short_name) {
+            return $post->transportPost->transportTrip->trip_short_name;
+        }
+
+        return '_';
+    }
+
+    private function createTrip(
+        Post $post,
+        LocationIdentifier $trwlOrigin,
+        LocationIdentifier $trwlDestination,
+    ): mixed {
+        $traewellingCategory = TransportMode::from($post->transportPost->transportTrip->mode)->getTraewellingType();
+
+        $stops = $post->transportPost->transportTrip->stops;
+        $stops = $stops->sortBy('stop_sequence');
+        $originStop = $stops->first();
+        $destinationStop = $stops->last();
+
+        $stopovers = $this->createStopovers($stops);
+
+        Log::debug('Creating Traewelling trip');
+
+        $requestBody = [
+            'originId' => (int) $trwlOrigin->identifier,
+            'originDeparturePlanned' => $originStop->departure_time?->toIso8601String() ?? $originStop->arrival_time?->toIso8601String() ?? '',
+            'destinationId' => (int) $trwlDestination->identifier,
+            'destinationArrivalPlanned' => $destinationStop->arrival_time?->toIso8601String() ?? $destinationStop->departure_time?->toIso8601String() ?? '',
+            'lineName' => $this->getLineName($post),
+            'journeyNumber' => $this->getJourneyNumber($post),
+            'operatorId' => null,
+            'category' => $traewellingCategory,
+            'stopovers' => array_values($stopovers->toArray()),
+        ];
+
+        Log::debug('Traewelling API create trip request', ['body' => $requestBody]);
+
+        $client = $this->getClient();
+        $response = $client->post('trains/trip', ['json' => $requestBody]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws Throwable
+     */
+    private function crosspostTransitous(Post $post): ?array
+    {
+        $this->getAccessToken($post);
+
+        $originStop = $post->transportPost->originStop;
+        $destinationStop = $post->transportPost->destinationStop;
+
+        $trwlOriginIdentifier = $this->getTrwlStationIdentifier($originStop->location);
+        $trwlDestinationIdentifier = $this->getTrwlStationIdentifier($destinationStop->location);
+        if (! $trwlOriginIdentifier || ! $trwlDestinationIdentifier) {
+            throw new Exception('Origin or destination not found');
+        }
+
+        // post to traewelling
+        $data = $this->checkin($post, $trwlOriginIdentifier, $trwlDestinationIdentifier);
+        if (isset($data['error'])) {
+            throw new Exception('Traewelling API error: '.$data['error']);
+        }
+
+        return $data;
+    }
+
+    private function getTrwlStationFromIdentifier(LocationIdentifier $identifier): ?array
     {
         $client = $this->getClient();
         $response = $client->get('stations', [
             'query' => [
-                'identifier' => $identifier,
+                'identifier' => $identifier->identifier,
                 'identifier_provider' => 'transitous',
             ],
         ]);
@@ -126,17 +329,23 @@ class TraewellingCrossCheckInJob implements ShouldQueue
      * @throws GuzzleException
      * @throws Throwable
      */
-    private function checkin(Post $post, LocationIdentifier $trwlOrigin, LocationIdentifier $trwlDestination, bool $force = false): mixed
-    {
+    private function checkin(
+        Post $post,
+        LocationIdentifier $trwlOrigin,
+        LocationIdentifier $trwlDestination,
+        bool $force = false,
+        ?string $tripId = null,
+    ): mixed {
         $client = $this->getClient();
         $body = [
             'body' => $post->body,
             'start' => $trwlOrigin->identifier,
             'destination' => $trwlDestination->identifier,
-            'tripId' => $post->transportPost->transportTrip->foreign_trip_id,
+            'tripId' => $tripId ?? $post->transportPost->transportTrip->foreign_trip_id,
             'departure' => $post->transportPost->originStop->departure_time?->toIso8601String(),
             'arrival' => $post->transportPost->destinationStop->arrival_time?->toIso8601String(),
-            'lineName' => $post->transportPost->transportTrip->line_name,
+            'lineName' => $this->getLineName($post),
+            'visibility' => $post->visibility->getTraewellingVisibility(),
             'force' => $force,
         ];
         try {
@@ -147,12 +356,27 @@ class TraewellingCrossCheckInJob implements ShouldQueue
             $response = $client->post('trains/checkin', ['json' => $body]);
         } catch (Throwable $e) {
             if ($e->getCode() === 409 && ! $force) {
-                return $this->checkin($post, $trwlOrigin, $trwlDestination, true);
+                return $this->checkin($post, $trwlOrigin, $trwlDestination, true, $tripId);
             } else {
                 throw $e;
             }
         }
 
         return json_decode($response->getBody()->getContents(), true);
+    }
+
+    private function getJourneyNumber(Post $post): ?int
+    {
+        $shortName = $post->transportPost->transportTrip->trip_short_name;
+        if ($shortName) {
+            if (is_numeric($shortName)) {
+                return (int) $shortName;
+            }
+            if (preg_match('/\d+/', $shortName, $matches)) {
+                return (int) $matches[0];
+            }
+        }
+
+        return null;
     }
 }
