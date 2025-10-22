4354dafbb478265669b4f48e7deab1a26dfada26
Levin <github@17d.me>
2025-10-22 21:21:55 +0200

    💄Remove svg -> lucide

--- resources/js/Pages/NewPostDialog/Partials/LocationListEntry.vue
+++ resources/js/Pages/NewPostDialog/Partials/LocationListEntry.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import LocationListEntryInfo from '@/Pages/NewPostDialog/Partials/LocationListEntryInfo.vue';
+import { getEmojiFromTags, getName } from '@/Services/LocationTypeService';
 import { LocationEntry } from '@/types';
 import { Link } from '@inertiajs/vue3';
+import { CircleQuestionMark } from 'lucide-vue-next';
 import { defineProps, ref } from 'vue';
-import {
-    getEmojiFromTags,
-    getName,
-} from '../../../Services/LocationTypeService';
 
 const props = defineProps({
     location: {
@@ -52,55 +50,15 @@ const linkData = ref({
                 {{ location!.distance }} m
             </div>
         </div>
-        <!--
-        for now, we don't need this button
-        <Link
-            v-if="showStartButton"
-            :href="route('posts.create.route')"
-            :data
-            class="btn btn-square btn-ghost"
-            as="button"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-[1.2em]"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
-                />
-            </svg>
-        </Link>
-        -->
         <LocationListEntryInfo :location>
             <template #activator="{ onClick }">
                 <button
                     class="btn btn-square btn-ghost"
                     @click.prevent="onClick"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="size-[1.2em]"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-                        />
-                    </svg>
+                    <CircleQuestionMark class="size-5" />
                 </button>
             </template>
         </LocationListEntryInfo>
     </Link>
 </template>
-
-<style scoped></style>
