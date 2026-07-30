ef24c5b64437347743103615ce077ab1508ea552
Levin <github@17d.me>
2026-07-30 19:33:36 +0200

    add ActivityPubActorHydrator and LikeControllerTest for remote likes handling

--- resources/js/Pages/Posts/Partials/UserListCard.vue
+++ resources/js/Pages/Posts/Partials/UserListCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import FollowButton from '@/Pages/Profile/Partials/FollowButton.vue';
 import router from '@/router';
+import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { UserDto } from '../../../../types/Api.gen';
 
@@ -10,7 +11,14 @@ const props = defineProps<{
     user: UserDto;
 }>();
 
+const isRemote = computed(() => !!props.user.profileUrl);
+
 function redirectToUser() {
+    if (isRemote.value) {
+        window.open(props.user.profileUrl!, '_blank', 'noopener,noreferrer');
+
+        return;
+    }
     router.push({
         name: 'profile.show',
         params: { username: props.user.username },
@@ -33,19 +41,31 @@ function redirectToUser() {
         </div>
     </div>
     <div class="list-col-grow ms-4" @click="redirectToUser">
-        <RouterLink :to="`/profile/${user.username}`">
+        <a
+            v-if="isRemote"
+            :href="user.profileUrl!"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <span class="font-bold hover:underline">
+                {{ user.name }}
+            </span>
+            <span class="mx-1 opacity-60">·</span>
+            <span class="text-xs opacity-60"> @{{ user.username }} </span>
+        </a>
+        <RouterLink v-else :to="`/profile/${user.username}`">
             <span class="font-bold hover:underline">
                 {{ user.name }}
             </span>
             <span class="mx-1 opacity-60">·</span>
             <span class="text-xs opacity-60"> @{{ user.username }} </span>
         </RouterLink>
-        <p class="opacity-60">
+        <p v-if="!isRemote" class="opacity-60">
             {{ user.statistics.followersCount }}
             {{ t('profile.stats.followers', user.statistics.followersCount) }}
         </p>
     </div>
     <div>
-        <FollowButton :user="user" />
+        <FollowButton v-if="!isRemote" :user="user" />
     </div>
 </template>
